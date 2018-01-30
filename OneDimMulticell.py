7d4c2fb24776e434339c78305159bbf458be245d
Adam Power <31990347+adampower48@users.noreply.github.com>
2018-01-30 15:45:13 +0000

    Created constructor for different Cell types. Changed calc_survive to use constructors instead of Cell subclasses.

--- OneDimMulticell.py
+++ OneDimMulticell.py
@@ -7,6 +7,16 @@ from Main import my_hash
 DEFAULT_WIDTH = 100
 START_FILE = "start_multi.txt"
 
+# Cell attributes
+CELL_TYPES = {
+    # (display_chr, debug_chr, neighbourhood, rules)
+    "Default": (chr(1), "1", Neighbourhoods1D.DEFAULT, Rules1D.DEFAULT),
+    "Far2": (chr(1132), "2", (-2, 2), Rules1D.DEFAULT),
+    "Far3": (chr(1421), "3", (-3, 3), Rules1D.DEFAULT),
+    "Left3": ("X", "4", (-3, -2, -1), {0: -1, 1: 0, 2: 1, 3: -1}),
+    "Abacus": ("O", "5", Neighbourhoods1D.ABACUS, Rules1D.ABACUS)
+}
+
 
 class Cell:
     # Base class for cell
@@ -21,7 +31,7 @@ class Cell:
             setattr(self, k, v)
 
     def type_of(self):
-        return self.neighbourhood, self.rules
+        return hash((self.neighbourhood, tuple(self.rules.items())))
 
     def __eq__(self, other):
         return self.neighbourhood == other.neighbourhood and self.rules == other.rules and self.alive == other.alive
@@ -30,48 +40,18 @@ class Cell:
         return hash((self.debug_chr, self.alive))
 
 
-class CellFar2(Cell):
-    def __init__(self):
-        super().__init__()
-        self.display_chr = chr(1132)
-        self.debug_chr = "2"
-        self.neighbourhood = (-2, 2)
+def build_cell(attr_tuple):
+    # Builds constructor of Cell given its attributes
+    return lambda: Cell(display_chr=attr_tuple[0],
+                        debug_chr=attr_tuple[1],
+                        neighbourhood=attr_tuple[2],
+                        rules=attr_tuple[3])
 
 
-class CellFar3(Cell):
-    def __init__(self):
-        super().__init__()
-        self.display_chr = chr(1421)
-        self.debug_chr = "3"
-        self.neighbourhood = (-3, 3)
-
-
-class CellLeft(Cell):
-    def __init__(self):
-        super().__init__()
-        self.display_chr = "X"
-        self.debug_chr = "4"
-        self.neighbourhood = (-3, -2, -1)
-        self.rules = {
-            0: -1,
-            1: 0,
-            2: 1,
-            3: -1
-        }
-
-
-class CellAbacus(Cell):
-    def __init__(self):
-        super().__init__()
-        self.display_chr = "O"
-        self.debug_chr = "5"
-        self.neighbourhood = Neighbourhoods1D.ABACUS
-        self.rules = Rules1D.ABACUS
-
-
-possible_types = (Cell, CellFar2, CellFar3, CellLeft, CellAbacus)
+# Constructors for Cell types
+possible_types = tuple(build_cell(CELL_TYPES[t]) for t in CELL_TYPES)
 possible_types_inst = tuple(map(lambda c: c(), possible_types))
-possible_weights = (1, 0, 0, 0, 0)
+possible_weights = (1, 1, 1, 1, 1)
 
 
 def read_start_state(filename=START_FILE):
@@ -115,7 +95,7 @@ def calc_survive(state, i):
     rules = state[i].rules
 
     num_living = 0
-    neighbour_counts = {type(state[i]): 0}
+    neighbour_counts = {state[i].type_of(): 0}
 
     # Counting
     for offset in neighbourhood:
@@ -125,14 +105,15 @@ def calc_survive(state, i):
             num_living += 1
 
             # Neighbour types
-            if type(state[adj]) in neighbour_counts:
-                neighbour_counts[type(state[adj])] += 1
+            if state[adj].type_of() in neighbour_counts:
+                neighbour_counts[state[adj].type_of()] += 1
             else:
-                neighbour_counts[type(state[adj])] = 1
+                neighbour_counts[state[adj].type_of()] = 1
 
-    # Class of new cell, weighted per number of cells
+    # Class of new cell, highest number priority
     max_count = max(neighbour_counts.values())
     max_type = choice([t for t in neighbour_counts if neighbour_counts[t] == max_count])
+    max_type = next(c for i, c in enumerate(possible_types) if possible_types_inst[i].type_of() == max_type)
 
     # Class of new cell, no weights
     # max_type = choice(tuple(neighbour_counts.keys()))
@@ -192,6 +173,6 @@ def run_complete(state):
 
 
 if __name__ == '__main__':
-    # state = gen_random_state()
-    state = read_start_state()
+    state = gen_random_state()
+    # state = read_start_state()
     state = run_complete(state)
